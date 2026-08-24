86c35fe5ccd09fbd3d24bfe4bc2d414fe09bcf49
Maria dos Anjos <mariadosanjos.108@gmail.com>
2026-08-24 15:49:53 +0100

    Estudando tipos de variáveis em Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 20;
+        short ano = 2023;
+        int cep = 12345678;
+        long cpf = 12345678901L;
+        final float PI = 3.14f;
+        double salarioMinimo = 2500;
+        boolean verdadeiro = true;
     }
 }
